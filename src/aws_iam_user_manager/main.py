aef685cd298985acd51371edf1e823ab9f4c4570
WP Swart <wpswart006@gmail.com>
2026-02-13 12:55:08 +0200

    Added some retry logic

--- src/aws_iam_user_manager/main.py
+++ src/aws_iam_user_manager/main.py
@@ -5,10 +5,13 @@ import tomllib
 import time
 import logging
 import time
+import functools
 
 from pathlib import Path
 
 logger = logging.getLogger()
+
+
 def init():
     with open("config.toml", "rb") as fp:
         config = tomllib.load(fp)
@@ -16,9 +19,13 @@ def init():
     log_destination = config.get("log_destination")
 
     if log_destination:
-        logging.basicConfig(filename=f"{log_destination}", format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+        logging.basicConfig(
+            filename=f"{log_destination}/{datetime.datetime.now().strftime("%Y-%m-%d___%H:%M:00")}",
+            format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+            level=logging.INFO,
+        )
     logger = logging.getLogger("init()")
-    
+
     template_file = config.get("template_file")
     if not template_file:
         logger.critical("No template file configured")
@@ -26,15 +33,29 @@ def init():
     if not Path(template_file).is_file():
         logger.critical("Template file not found")
         sys.exit()
-    os.environ["template_file"]=template_file
+    os.environ["template_file"] = template_file
 
     output_file = config.get("output_file")
     if not output_file:
         logger.critical("No output file configured")
         sys.exit()
-    
-    os.environ["output_file"]=output_file
 
+    os.environ["output_file"] = output_file
+
+
+def retry(func, tries=1):
+    exc = None
+    logger = logging.getLogger("retry()")
+    while tries > 0:
+        try:
+            logger.info(f"trying... {tries} left")
+            result = func()
+            return result
+        except Exception as e:
+            tries -= 1
+            exc = e
+
+    raise exc
 
 
 def main() -> None:
@@ -48,57 +69,52 @@ def main() -> None:
     iam = session.client("iam")
     sts = session.client("sts")
 
-    username = sts.get_caller_identity()["Arn"].split("/")[-1]
+    username = retry(sts.get_caller_identity, 3)["Arn"].split("/")[-1]
 
-    access_key_id = session.get_credentials().access_key
+    access_key_id = retry(session.get_credentials, 3).access_key
 
-    response = iam.list_access_keys(UserName=username)
-    
+    response = retry(functools.partial(iam.list_access_keys, UserName=username), 3)
+
     for k in filter(lambda x: x["Status"] == "Inactive", response["AccessKeyMetadata"]):
-        logger.info("Disabling access key with id")
-        iam.delete_access_key(
-            UserName=k["UserName"],
-            AccessKeyId=k["AccessKeyId"],
+        logger.info("Deleting access key with id")
+        retry(
+            functools.partial(
+                iam.delete_access_key,
+                UserName=k["UserName"],
+                AccessKeyId=k["AccessKeyId"],
+            ),
+            3,
         )
-    
 
     logging.info("Creating new access key")
-    response = iam.create_access_key(
-        UserName=username
-    )
+    response = retry(functools.partial(iam.create_access_key, UserName=username), 3)
 
-    new_access_key=response["AccessKey"]
+    new_access_key = response["AccessKey"]
     new_access_key_id = new_access_key["AccessKeyId"]
     new_secret_access_key = new_access_key["SecretAccessKey"]
 
-
-    
     with open(os.environ["template_file"], "r") as fp:
         template = fp.read()
 
-    template = (
-        template.replace("${AWS_ACCESS_KEY_ID}", new_access_key_id)
-        .replace("${AWS_SECRET_ACCESS_KEY}", new_secret_access_key)
+    template = template.replace("${AWS_ACCESS_KEY_ID}", new_access_key_id).replace(
+        "${AWS_SECRET_ACCESS_KEY}", new_secret_access_key
     )
-
-
 
     logging.info("Updating env")
     with open(os.environ["output_file"], "w") as fp:
         fp.write(template)
 
     logging.info("Disable old access key")
-    response = iam.update_access_key(
-        UserName=username,
-        AccessKeyId = access_key_id,
-        Status = "Inactive"
+    response = retry(
+        functools.partial(
+            iam.update_access_key,
+            UserName=username,
+            AccessKeyId=access_key_id,
+            Status="Inactive",
+        ),
+        3,
     )
     logging.info("Done")
-        
-
-
-    
-
 
 
 if __name__ == "__main__":
